daaf3eba84d9c6e0cec0d6e21fb20d636bdcb23a
Juan Trebolle <juan.itrebolle@gmail.com>
2021-04-28 22:15:19 +0200

    Add Splash screen(needs bigger size)

--- app/src/main/java/com/example/e_sneaker/MainActivity.java
+++ app/src/main/java/com/example/e_sneaker/MainActivity.java
@@ -8,6 +8,14 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        /*I'll use SplashTheme in the moment that the app is loading, and the default theme(ESneaker)
+        * when it's already loaded*/
+        setTheme(R.style.Theme_ESneaker);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
