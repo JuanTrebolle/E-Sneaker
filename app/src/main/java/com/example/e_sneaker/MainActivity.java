0fb527753874b2ec2bb2dcc478bf47a3c446c7ad
Juan Trebolle <juan.itrebolle@gmail.com>
2021-05-01 13:23:02 +0200

    Add Toolbar & toolbar menu - no functionality yet
    the toolbar menu contains the items: search, profile and settings(no functionality).
    
    The Toolbar has a problem, it shows app underneath the default app, which shouldn't happen because I deleted it from the theme, but for some reason, it  shows up both bars.

--- app/src/main/java/com/example/e_sneaker/MainActivity.java
+++ app/src/main/java/com/example/e_sneaker/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.e_sneaker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,5 +23,31 @@ public class MainActivity extends AppCompatActivity {
         setTheme(R.style.Theme_ESneaker);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar); // With this the app crashes
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId();
+
+        if (itemId == R.id.search){
+            //TODO: call viewModel to search specific shoe
+            Toast.makeText(this, "Search", Toast.LENGTH_SHORT).show();
+        } else if (itemId == R.id.profile){
+            //TODO: call viewModel to enter profile settings
+            Toast.makeText(this, "Profile", Toast.LENGTH_SHORT).show();
+        } else if (itemId == R.id.settings){
+            //TODO: call viewModel to enter settings
+            Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
