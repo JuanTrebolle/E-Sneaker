ba34a9a9f2497e7b6fc961d937c2a03c62db3191
Juan Trebolle <juan.itrebolle@gmail.com>
2021-05-04 19:31:30 +0200

    Start activities after login

--- app/src/main/java/com/example/e_sneaker/Login.java
+++ app/src/main/java/com/example/e_sneaker/Login.java
@@ -2,7 +2,9 @@ package com.example.e_sneaker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void login(View view) {
+        startActivity(new Intent(Login.this, MainActivity.class));
+    }
+
+    public void register(View view) {
+        //startActivity(new Intent(Register.this, MainActivity.class));
     }
 }
