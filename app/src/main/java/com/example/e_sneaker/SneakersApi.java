d573e8c3345a919c854eb79b476ab48f67ec922b
Juan Trebolle <juan.itrebolle@gmail.com>
2021-05-13 16:15:10 +0200

    Changes SneakerApi

--- app/src/main/java/com/example/e_sneaker/SneakersApi.java
+++ app/src/main/java/com/example/e_sneaker/SneakersApi.java
@@ -1,5 +1,7 @@
 package com.example.e_sneaker;
 
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
@@ -7,11 +9,12 @@ import retrofit2.http.Path;
 public interface SneakersApi {
 
     //@GET("https://v1-sneakers.p.rapidapi.com/v1/sneakers")
-    @GET("api/v1/sneakers")
-    Call<SneakerResponse> getAllSneakers();
 
-    @GET("api/v1/{brand}")
-    Call<SneakerResponse> getSneakersByBrand(@Path("brand") String brand);
+    /*@GET("api/v1/{brand}")
+    Call<List<Sneaker>> getSneakersByBrand(@Path("brand") String brand);*/
+
+    @GET("api/v1/sneakers")
+    Call<List<Sneaker>> getAllSneakers(@Path("limit") int limit);
 
     /*@GET("api/v1/sneakers/{name}")
     Call<SneakerResponse> getSneakerByName(@Path("name") String name);*/
